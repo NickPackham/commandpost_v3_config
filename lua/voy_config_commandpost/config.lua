ce9828b3374b9c2470f9d7ef5d91542d98cc0c66
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-05-15 01:08:28 -0700

    Create config.lua

---
+++ lua/voy_config_commandpost/config.lua
@@ -0,0 +1,102 @@
+VOY_GM_CommandPost = {}
+
+VOY_GM_CommandPost_CISJobs = {
+	[TEAM_GUN] = true,
+}
+
+VOY_GM_CommandPost_Staff = {
+	["superadmin"] = true,
+	["admin"] = true,
+}
+
+--[[
+
+	Names that will be randomly picked for the Command Posts.
+
+--]]
+
+VOY_GM_CommandPost_Names = {
+	"Alpha",
+	"Charlie",
+	"Tango",
+	"Delta",
+	"Golf",
+}
+
+--[[
+
+	Republic Voicelines
+
+--]]
+
+VOY_GM_CommandPost_RepublicVoicelines_Capture = {
+	"commandpost/republic/claimpost1.wav",
+	"commandpost/republic/claimpost2.wav",
+	"commandpost/republic/claimpost3.wav",
+}
+
+VOY_GM_CommandPost_RepublicVoicelines_Lost = {
+	"commandpost/republic/lostclaimpost1.wav",
+	"commandpost/republic/lostclaimpost2.wav",
+}
+
+--[[
+
+	CIS Voicelines
+
+--]]
+
+VOY_GM_CommandPost_CISVoicelines_Capture = {
+	"commandpost/cis/claimpost1.wav",
+	"commandpost/cis/claimpost2.wav",
+	"commandpost/cis/claimpost3.wav",
+}
+
+VOY_GM_CommandPost_CISVoicelines_Lost = {
+	"commandpost/cis/lostclaimpost1.wav",
+	"commandpost/cis/lostclaimpost2.wav",
+}
+
+--[[
+
+	Stuff
+
+--]]
+
+VOY_GM_CommandPost_Announcer = true -- Clone and CIS Announcer
+
+VOY_GM_CommandPost_MachineSounds = true -- Should the Command Post play looping Machine noises?
+
+VOY_GM_CommandPost_Spawn = true -- Should CIS Jobs spawn at CIS Control Points and Vice-Versa?
+
+--[[
+
+	Models
+
+--]]
+
+VOY_GM_CommandPost_NeutralModel = "models/capturepoint/white_none/base.mdl"
+
+VOY_GM_CommandPost_RepublicModel = "models/capturepoint/blue/republic/republic_b.mdl"
+
+VOY_GM_CommandPost_CISModel = "models/capturepoint/red/cis/cis_r.mdl"
+
+--[[
+
+	Language
+
+--]]
+
+VOY_GM_CommandPost_LangOne = "You don't have permission to use this Command." -- You don't have permission to use this Command.
+
+VOY_GM_CommandPost_LangTwo = "You aren't looking at a Command Post." -- You aren't looking at a Command Post.
+
+VOY_GM_CommandPost_LangThree = "Command Post" -- Command Post
+
+VOY_GM_CommandPost_LangFour = "Was taken by" -- Was taken by
+
+VOY_GM_CommandPost_LangFive = "Republic" -- Republic
+
+VOY_GM_CommandPost_LangSeven = "CIS" -- CIS
+
+VOY_GM_CommandPost_LangSix = "Forces" -- Forces
