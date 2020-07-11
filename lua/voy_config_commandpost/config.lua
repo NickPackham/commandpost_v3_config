e7b3824d986dc6960835111f516993920c78584c
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-07-11 05:31:55 -0700

    Update config.lua

--- lua/voy_config_commandpost/config.lua
+++ lua/voy_config_commandpost/config.lua
@@ -1,76 +1,69 @@
-VOY_GM_CommandPost = {} -- This neither
+EPS_CommandPost_Config = {
+	Interval = 5, -- Interval of the timer, basically every x seconds it will update the claim percentage.
 
---[[
+	AmbientSound = true, -- Should the Command Post play looping Machine noises?
 
-	Main Configuration
+	MaxClaimPoints = 100, -- Max Claim Percentage, 0 - this number
 
---]]
+	--[[
 
-VOY_GM_CommandPost_TimeClaim = 5 -- Interval of the timer, basically every x seconds it will update the claim percentage.
+		Rewards Configuration
 
-VOY_GM_CommandPost_StartPoints = 50 -- Claim Percentage it should start off at.
+	--]]
 
-VOY_GM_CommandPost_MachineSounds = true -- Should the Command Post play looping Machine noises?
+	RequiredPlayers = 1, -- How many players are required for Players to be paid for capturing command posts? (Set to false to allow this module no matter how many players are online.)
 
-VOY_GM_CommandPost_MaxClaimPoints = 100 -- Max Claim Percentage, 0 - this number
+	DefaultFaction = "Republic", -- Default Faction which each Command Post should start out as.
 
-VOY_GM_CommandPost_RespawnTime = 5 -- How long should someone wait to respawn? (disable by entering false or 0) (only enabled if Faction Command Post spawns are enabled.)
+	CommandPost_Names = {"Golf"},
 
---[[
+	["Factions"] = {
+		["CIS"] = {
+			Color = Color(255,0,0),
+			Model = "models/capturepoint/red/cis/cis_r.mdl",
+			Jobs = {"Gun Dealer", "Gun Dealer 2"},
+			Capture = {"commandpost/cis/claimpost1.wav", "commandpost/cis/claimpost2.wav", "commandpost/cis/claimpost3.wav"}, -- Gained Voice Lines (Announcer VO)
+			Lost = {"commandpost/cis/lostclaimpost1.wav", "commandpost/cis/lostclaimpost2.wav"}, -- Lost Voice Lines (Announcer VO)
+			Max = 25, -- Percentile Form, lower and higher.
+			Minimum = 0, -- Percentile Form, lower and higher.
+			DeathSpawn = true, -- Should this Faction be able to respawn at their claimed command posts.
+			RespawnTime = 5,
+			ShowHud = true,
+			ShowClaim = true,
 
-	Rewards Configuration
+			ClaimMoney = 0,
+		},
+		["Republic"] = {
+			Color = Color(0,0,255),
+			Model = "models/capturepoint/blue/republic/republic_b.mdl",
+			Jobs = {"Citizen", "Citizen 2"},
+			Capture = {"commandpost/republic/claimpost1.wav", "commandpost/republic/claimpost2.wav", "commandpost/republic/claimpost3.wav"},-- Gained Voice Lines (Announcer VO)
+			Lost = {"commandpost/republic/lostclaimpost1.wav", "commandpost/republic/lostclaimpost2.wav"}, -- Lost Voice Lines (Announcer VO)
+			Max = 100, -- Percentile Form, lower and higher.
+			Minimum = 75, -- Percentile Form, lower and higher.
+			DeathSpawn = true, -- Should this Faction be able to respawn at their claimed command posts.
+			RespawnTime = 5,
+			ShowHud = true,
+			ShowClaim = true,
 
---]]
+			ClaimMoney = 200,
 
-VOY_GM_CommandPost_ClaimMoney_Players = 5 -- How many players are required for Players to be paid for capturing command posts? (Set to false to allow this module no matter how many players are online.)
+		},
+		["Neutral"] = {
+			Color = Color(255,255,255),
+			Model = "models/capturepoint/white_none/base.mdl",
+			Jobs = {""},
+			Capture = {""},
+			Lost = {""},
+			Max = 75, -- Percentile Form, lower and higher.
+			Minimum = 50, -- Percentile Form, lower and higher.
+			DeathSpawn = false, -- Should this Faction be able to respawn at their claimed command posts.
+			RespawnTime = 5,
+			ShowHud = true,
+			ShowClaim = false,
 
-VOY_GM_CommandPost_ClaimMoney = 1000 -- How much money should they be paid for capturing a Command Post? (set to false or 0 to disable.)
+			ClaimMoney = 0,
 
---[[
-
-	Main Configuration PT2
-
---]]
-
-VOY_GM_CommandPost_NonHud = { -- Which factions shouldn't have a HUD for Command Posts?
-	["Neutral"] = true,
-}
-
-VOY_GM_CommandPost_NoShow = { -- Which factions shouldn't have a message for them claiming a Command Post?
-	["Neutral"] = true,
-}
-
-VOY_GM_CommandPost_Factions = {
-	["CIS"] = {
-		Color = Color(255,0,0),
-		Model = "models/capturepoint/red/cis/cis_r.mdl",
-		Jobs = {"Gun Dealer", "Gun Dealer 2"},
-		Capture = {"commandpost/cis/claimpost1.wav", "commandpost/cis/claimpost2.wav", "commandpost/cis/claimpost3.wav"}, -- Gained Voice Lines (Announcer VO)
-		Lost = {"commandpost/cis/lostclaimpost1.wav", "commandpost/cis/lostclaimpost2.wav"}, -- Lost Voice Lines (Announcer VO)
-		Max = 0, -- Percentile Form, lower and higher.
-		Minimum = 25, -- Percentile Form, lower and higher.
-		DeathSpawn = true, -- Should this Faction be able to respawn at their claimed command posts.
-	},
-	["Republic"] = {
-		Color = Color(0,0,255),
-		Model = "models/capturepoint/blue/republic/republic_b.mdl",
-		Jobs = {"Citizen", "Citizen 2"},
-		Capture = {"commandpost/republic/claimpost1.wav", "commandpost/republic/claimpost2.wav", "commandpost/republic/claimpost3.wav"},-- Gained Voice Lines (Announcer VO)
-		Lost = {"commandpost/republic/lostclaimpost1.wav", "commandpost/republic/lostclaimpost2.wav"}, -- Lost Voice Lines (Announcer VO)
-		Max = 100, -- Percentile Form, lower and higher.
-		Minimum = 75, -- Percentile Form, lower and higher.
-		DeathSpawn = true, -- Should this Faction be able to respawn at their claimed command posts.
-	},
-	["Neutral"] = {
-		Color = Color(255,255,255),
-		Model = "models/capturepoint/white_none/base.mdl",
-		Jobs = {""},
-		Capture = {""},
-		Lost = {""},
-		Max = 25, -- Percentile Form, lower and higher.
-		Minimum = 75, -- Percentile Form, lower and higher.
-		DeathSpawn = false, -- Should this Faction be able to respawn at their claimed command posts.
+		},
 	},
 }
-
-VOY_GM_CommandPost_Names = {"Alpha", "Charlie", "Tango", "Delta", "Golf"} -- Random Names for the Command Posts, feel free to put more here.
